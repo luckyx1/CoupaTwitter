41ecda1ade6255e41393e1a7c89de03c78e15326
ROBERT HERNANDEZ <robert.hernandez@coupa.com>
2014-10-27 11:59:43 -0700

    initalize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoupaTwitter::Application.config.secret_token = '036d2d091d9d63701f463c5bef0b250dd24403f5024f271ff784eab66499d2fc0988bee07d6c325aa7cde8284160bf31ceeb243bdb2526d659eee883a33217bb'
