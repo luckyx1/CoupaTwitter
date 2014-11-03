610f361fd8f320f77dbf8b24eb6482dac916badb
ROBERT HERNANDEZ <robert.hernandez@coupa.com>
2014-11-03 10:49:12 -0800

    new test files

---
+++ lib/rubocop/Cop/deprications/dynamic_finder.rb
@@ -0,0 +1,44 @@
+# encoding: utf-8
+
+module RuboCop
+  module Cop
+    module deprications
+      class DynamicFinder < Cop
+        #node.loc.expression.source
+
+        def on_send(node)
+          _receiver, method_name, *_args = *node
+          if (depricated_method(method_name) && (depricated_condition(method_name)) || method_name.to_s.include? 'scoped'
+            val = method_name.to_s
+            if val.include? '_all_' || val.include? 'scoped_by'
+              msg = '`%s` should be replaced with `where(%s)`. Please use the latter.'
+            elsif val.include? 'initialize'
+              msg = '`%s` should be replaced with `find_or_initialize_by(%s)`. Please use the latter.'
+            elsif val.include? 'create'
+              msg = '`%s` should be replaced with `find_or_create_by(%s)`. Please use the latter.'
+            elsif val.include? 'last'
+              msg = '`%s` should be replaced with `where(%s).last`. Please use the latter.'
+            end
+            add_offense(node,
+                      :expression,
+                      format(msg, method_name, format_elm(method_name)))
+          end
+        end
+
+        def depricated_method(elm)
+          val = elm.to_s
+          return val.include? 'find' 
+        end
+
+        def depricated_condition(elm)
+          val = elm.to_s
+          return ((val.include? '_all_') || (val.include? '_or_') || (val.include? 'last'))
+        end
+
+        def format_elm(elm)
+          val = elm.to_s
+          i = val.index('by')
+          return val[i+3,val.size]
+        end
+  end
+end
---
+++ lib/rubocop/Cop/deprications/rjsdeprication.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+module RuboCop
+  module Cop
+    module deprications
+      class Rjsdeprication < Cop
+        #node.loc.expression.source
+
+        def on_send(node)
+          _receiver, method_name, *_args = *node
+          if method_name.to_s.include? 'render'
+            #p arg.last.loc.expression
+            msg = "caught"
+            add_offense(node,
+                      :expression,
+                      msg)
+          end
+        end
+  end
